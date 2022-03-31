19714712b7120f1f09c7e4b153e0fb3f5624fd2e
Alex <keeper.gen@gmil.com>
2022-03-31 23:13:33 +0300

    feat: added Reset and Show Origin functions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 
 const Counter = (props) => {
-	const [value, setValue] = useState(props.value);
+	//const [value, setValue] = useState(props.value);
+	const {value} = props
 
 	const formatValue = () => {
 		return value === 0 ? 'empty' : value;
@@ -14,10 +15,12 @@ const Counter = (props) => {
 	};
 
 	const handleIncrement = () => {
-		setValue((prevState) => prevState + 1);
+		console.log('handle increment');
+		// setValue((prevState) => prevState + 1);
 	};
 	const handleDecrement = () => {
-		setValue((prevState) => prevState - 1);
+		console.log('handle decrement');
+		// setValue((prevState) => prevState - 1);
 	};
 
 	return (
--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -2,17 +2,31 @@ import React, {useState} from "react";
 import Counter from "./counter";
 
 const CountersList = () => {
-	const [counters, setCounters] = useState([
+	const initialState = [
 		{id: 0, value: 0, name: 'Нож'},
 		{id: 1, value: 0, name: 'Ложка'},
 		{id: 2, value: 3, name: 'Вилка'},
 		{id: 3, value: 0, name: 'Тарелка'},
 		{id: 4, value: 0, name: 'Чашка'}
-	]);
+	];
+	const [counters, setCounters] = useState(initialState);
 	const handleDelete = (id) => {
 		const newCounters = counters.filter(counter => counter.id !== id);
 		setCounters(newCounters);
 	};
+	const handleReset = () => {
+		setCounters(initialState);
+	}
+	const handleUpdate = () => {
+		const updatedState = [
+			{id: 0, value: 4, name: 'Нож'},
+			{id: 1, value: 4, name: 'Ложка'},
+			{id: 2, value: 4, name: 'Вилка'},
+			{id: 3, value: 4, name: 'Тарелка'},
+			{id: 4, value: 0, name: 'Чашка'}
+		];
+		setCounters(updatedState);
+	}
 	return (
 		<>
 			{counters.map((count) => (
@@ -21,6 +35,8 @@ const CountersList = () => {
 					onDelete={handleDelete}
 					{...count}
 				/>))}
+			<button className="btn btn-primary btn-sm m-2" onClick={handleReset}>Reset</button>
+			<button className="btn btn-primary btn-sm m-2" onClick={handleUpdate}>Show Origin</button>
 		</>
 	);
 };
