1f9c25a113524df024a98554d17044c1dac22263
Alex <keeper.gen@gmil.com>
2022-03-31 16:27:08 +0300

    refactor: removing render tags code

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 
 const Counter = () => {
 	const [count, setCount] = useState(0);
-	const [tags, setTags] = useState(['Первый', 'Второй', 'Третий']);
 
 	const formatCount = () => {
 		return count === 0 ? 'empty' : count;
@@ -16,24 +15,10 @@ const Counter = () => {
 
 	const handleIncrement = () => {
 		setCount((prevState) => prevState + 1);
-	}
+	};
 	const handleDecrement = () => {
 		setCount((prevState) => prevState - 1);
-	}
-
-	const handleTagChange = (id) => {
-		setTags(prevState => prevState.filter(tag => tag !== id));
-		console.log(id);
 	};
-
-	const renderTags = () => {
-		return tags.length !== 0 &&
-			tags.map(tag => (<li key={tag} className={"btn btn-primary btn-sm m-2"} onClick={() => handleTagChange(tag)}>{tag}</li>))
-	};
-
-	if (tags.length !== 0) {
-		return <ul>{renderTags()}</ul>
-	}
 
 	return (
 		<>
