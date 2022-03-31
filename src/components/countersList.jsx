3f15b85e4f6a5c426dac3a5260ed83e5d982361b
Alex <keeper.gen@gmil.com>
2022-03-31 17:21:14 +0300

    feat: creating several counters with unique names

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -3,14 +3,15 @@ import Counter from "./counter";
 
 const CountersList = () => {
 	const counters = [
-		{id: 0, value: 0},
-		{id: 1, value: 0},
-		{id: 2, value: 3},
-		{id: 3, value: 0},
-		{id: 4, value: 0}];
+		{id: 0, value: 0, name: 'Нож'},
+		{id: 1, value: 0, name: 'Ложка'},
+		{id: 2, value: 3, name: 'Вилка'},
+		{id: 3, value: 0, name: 'Тарелка'},
+		{id: 4, value: 0, name: 'Чашка'}
+	];
 	return (
 		<>
-			{counters.map(count => <Counter key={count.id} value={count.value} />)}
+			{counters.map(count => <Counter key={count.id} value={count.value} name={count.name} />)}
 		</>
 	);
 };
