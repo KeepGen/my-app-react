da8e120010819ee7d0a4e306b177ba93a8c6d792
Alex <keeper.gen@gmil.com>
2022-03-31 16:55:08 +0300

    feat: Component Composition

---
+++ src/components/countersList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Counter from "./counter";
+
+const CountersList = () => {
+	const counters = [
+		{id: 0, value: 0},
+		{id: 1, value: 0},
+		{id: 2, value: 3},
+		{id: 3, value: 0},
+		{id: 4, value: 0}];
+	return (
+		<>
+			{counters.map(count => <Counter key={count.id} />)}
+		</>
+	);
+};
+
+export default CountersList
